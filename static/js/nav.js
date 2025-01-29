58779e0314474a9822b6ac547b4c5b21e88a2670
mannarbahy <mannar.bahyy@gmail.com>
2025-01-29 08:00:02 +0200

    add user authantication

--- static/js/nav.js
+++ static/js/nav.js
@@ -13,3 +13,12 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger');
+    const menu = document.getElementById('menu');
+  
+    hamburger.addEventListener('click', function () {
+      menu.classList.toggle('active');
+    });
+  });
